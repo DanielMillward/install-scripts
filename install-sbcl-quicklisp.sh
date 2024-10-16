12f38828ef15fc681cec6234837637c2eff0e84f
Daniel Millward <dfmillward02@gmail.com>
2024-10-16 11:21:42 -0600

    Add installing sbcl and quicklisp

---
+++ install-sbcl-quicklisp.sh
@@ -0,0 +1,10 @@
+# https://lisp-lang.org/learn/getting-started/
+
+sudo apt-get install sbcl
+
+curl -o /tmp/ql.lisp http://beta.quicklisp.org/quicklisp.lisp
+
+sbcl --no-sysinit --no-userinit --load /tmp/ql.lisp \
+       --eval '(quicklisp-quickstart:install :path "~/.quicklisp")' \
+       --eval '(ql:add-to-init-file)' \
+       --quit
